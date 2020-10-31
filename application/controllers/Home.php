8765ae86e9831134729cedc2154cd1b7ff0ca7de
iqbalfebian <iqbalfebian.krw@gmail.com>
2020-10-31 22:34:00 +0700

    update home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,7 +5,11 @@ class Home extends CI_Controller {
 
   public function index()
   {
-    $this->load->view('homepage/homepage.php');
+    $data['title'] = 'Toko Online';
+    $data1['barang'] = $this->model_barang->tampil_data()->result();
+    $this->load->view('templates/header', $data);
+    $this->load->view('templates/homepage', $data1);
+    $this->load->view('templates/footer');
   }
 
 }
