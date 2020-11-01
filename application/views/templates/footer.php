b90d5c3b0072f4f4ef088942409bc163a88b93f0
iqbalfebian <iqbalfebian.krw@gmail.com>
2020-11-01 09:54:35 +0700

    edit dikit footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer class="py-5 bg-dark">
   <div class="container">
-    <p class="m-0 text-center text-white">Copyright &copy; Inti Warna Bangunan</p>
+    <p class="m-0 text-center text-white">Copyright &copy; <?= date('Y'); ?> Inti Warna Bangunan</p>
   </div>
   <!-- /.container -->
 </footer>
