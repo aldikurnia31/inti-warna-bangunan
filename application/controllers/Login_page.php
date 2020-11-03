b3ec3c677677023fc9d5029331a1d34d67086cff
aldikurnia <aldikurnia161@gmail.com>
2020-11-03 10:45:32 +0700

    Menambah Fitur Login dan Register Sementara (Masih ada Error) Kalau keburu Benerin

--- application/controllers/Login_page.php
+++ application/controllers/Login_page.php
@@ -23,4 +23,71 @@ class Login_page extends CI_Controller
             $this->_login();
         }
     }
+
+    private function _login(){
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('admin', ['email' => $email])->row_array();
+
+        //jika user ada
+        if($user){
+            //Jika usernya aktif
+            if($user['is_active'] == 1){
+                // cek passwordnya
+                if(password_verify($password, $user['password'])){
+                    $data = [
+                        'email' => $user['email'],
+                        'role_id' => $user['role_id']
+                    ];
+                    $this->session->set_userdata($data);
+                    redirect('admin');
+                
+                } else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Password Salah!</div>');
+                    redirect('login_page');
+                }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">This email has not been activated!</div>');
+                redirect('login_page');
+            }
+
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email is not rehistered </div>');
+            redirect('login_page');
+        }
+    }
+
+    public function registrasi(){
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email','Email','required|trim|valid_email|is_unique[admin.email]', [
+            'is_unique' => 'Email ini sudah terdaftar!' 
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]',[
+            'matches' => 'Password dont match!',
+            'min_length' => 'Password too short!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+        if($this->form_validation->run() == false){
+            
+            $data['title'] = 'Register Page';
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/templates_login/register');
+            $this->load->view('templates/footer');
+        } else {
+            $data = [
+                'name' => htmlspecialchars($this->input->post('name', true)),
+                'email' => htmlspecialchars($this->input->post('email', true)),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('admin', $data); //user itu nama table di databasenya
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation! your account has been created. Please Login </div>');
+            redirect('login_page');
+        }
+    }
 }
